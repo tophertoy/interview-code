9dd7d3ae0ddb0d7b01cc858be36452bd18dfc57b
Chris Toynbee <ctoynbee@gmail.com>
2021-10-27 16:00:39 +1300

    js examples to review

---
+++ examples.js
@@ -0,0 +1,46 @@
+// Example 1
+
+// Notifies the user
+function notif(user) {
+  // implementation
+}
+
+// Example 2
+let daysSLV = 10;
+let y = new Date().getFullYear();
+
+let ok;
+if (user.age > 30) {
+  ok = true;
+}
+
+// Example 3
+const users = ["John", "Marco", "Peter"];
+users.forEach(u => {
+  doSomething();
+  doSomethingElse();
+  // ...
+  // ...
+  // ...
+  // ...
+  register(u);
+});
+
+
+// Example 4
+const user = {
+  userName: "John",
+  userSurname: "Doe",
+  userAge: "28"
+};
+
+// ....
+// ....
+// ....
+
+user.userName;
+
+// Example 5
+function getUsers(fields, fromDate, toDate, limit, userType) {
+  // implementation
+}
